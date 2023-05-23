b65043493c38a05437526ab02c98304dbeda2068
zijadddd <zijad.doglod.20@size.ba>
2023-05-23 18:21:41 +0200

    User registration test added

--- KKS_2022_23_Automated_Testing_With_Selenium/Helpers/StaticMethods.cs
+++ KKS_2022_23_Automated_Testing_With_Selenium/Helpers/StaticMethods.cs
@@ -1,18 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KKS_2022_23_Automated_Testing_With_Selenium.Helpers {
+﻿namespace KKS_2022_23_Automated_Testing_With_Selenium.Helpers {
     static class StaticMethods {
-        public static bool CheckIfElementExists(IWebDriver webDriver, string elementId) {
+
+        public static bool CheckIfElementExistsByLinkText(IWebDriver webDriver, string text) {
+            try {
+                webDriver.FindElement(By.LinkText(text));
+                return true;
+            } catch (NoSuchElementException) {
+                return false;
+            }
+        }
+
+        public static bool CheckIfElementExistsById(IWebDriver webDriver, string elementId) {
             try {
                 webDriver.FindElement(By.Id(elementId));
                 return true;
             } catch (NoSuchElementException) {
                 return false;
             }
+        }
+
+        public static void Login() {
+
         }
     }
 }
